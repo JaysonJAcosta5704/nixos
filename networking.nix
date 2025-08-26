7d1334076004d25ec534a7268bc9e07764632d7b
Jayson Acosta <JaysonJAcosta5704.coding@gmail.com>
2025-08-26 07:02:46 -0500

    Added support for objects to allow special networks, such as eduroam

--- networking.nix
+++ networking.nix
@@ -1,20 +1,26 @@
-{
-  ...
-}:
-let 
+{ ... }:
+
+let
   wifiPasswords = import ./secrets/WifiSecrets.nix;
   ssidList = builtins.attrNames wifiPasswords;
+
   wifiNetworkAttrs = builtins.listToAttrs (
-    map (ssid: {
-      name = ssid;
-      value = { psk = wifiPasswords.${ssid}; };
-    }) ssidList
+    map (ssid:
+      let
+        entry = wifiPasswords.${ssid};
+      in {
+        name = ssid;
+        value = if builtins.isString entry then { psk = entry; } else entry;
+      }
+    ) ssidList
   );
 in {
-  networking.hostName = "Jayson-Thinkpad-T460";
-  networking.wireless = {
-    enable = true;
-    userControlled.enable = true;
-    networks = wifiNetworkAttrs;
+  networking = {
+    hostName = "Jayson-Thinkpad-T480s";
+    wireless = {
+      enable = true;
+      userControlled.enable = true;
+      networks = wifiNetworkAttrs;
+    };
   };
 }
