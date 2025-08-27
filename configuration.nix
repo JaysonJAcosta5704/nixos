3d66f1d437e03e70425eb46407dbcea8b580ab82
Jayson Acosta <JaysonJAcosta5704.coding@gmail.com>
2025-08-27 01:34:17 -0500

    Add OSProber for duel booted systems, added plymouth theme, added regreet with theme

--- configuration.nix
+++ configuration.nix
@@ -3,10 +3,10 @@
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 {
+  config,
   pkgs,
   ...
 }:
-
 {
   imports = [
     ./networking.nix
@@ -14,13 +14,39 @@
     <home-manager/nixos>
   ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.grub.device = "nodev";
+  environment.systemPackages = with pkgs; [
+    adwaita-icon-theme
+    gnome-themes-extra
+    sway
+  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.plymouth.enable = true;
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+        extraEntries = ''
+          menuentry "Shutdown" { halt }
+          menuentry "Reboot" { reboot }
+          menuentry "Enter BIOS" { fwsetup }
+          menuentry "Rescue Shell" {
+            linux /boot/bzImage
+            initrd /boot/initrd
+            boot
+          }
+'';
+      };
+      efi.efiSysMountPoint = "/boot";
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    plymouth = {
+      enable = true;
+      theme = "spinner";
+      themePackages = with pkgs; [ adi1090x-plymouth-themes ];
+    };
+  };
 
   time.timeZone = "America/Chicago";
   i18n.defaultLocale = "en_US.UTF-8";
@@ -40,7 +66,20 @@
     ];
   };
 
+  users.users.greeter = {
+    isSystemUser = true;
+    description = "Greeter user for ReGreet";
+    home = "/var/lib/greeter";
+    createHome = true;
+    shell = pkgs.zsh;
+    group = "greeter";
+  };
+
   programs.zsh.enable = true;
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 
   security.rtkit.enable = true;
   security.polkit.enable = true;
@@ -62,11 +101,34 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-        user = "Jayson";
+        command = "${pkgs.cage}/bin/cage -s -- ${config.programs.regreet.package}/bin/regreet";
+        user = "greeter";
       };
     };
   };
+
+programs.regreet = {
+  enable = true;
+  settings = {
+    default_session = "sway";
+    background = {
+      path = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray}/share/backgrounds/nixos/nix-wallpaper-nineish-dark-gray.png";
+      fit = "Cover";
+    };
+    
+    theme = {
+      name = "Adwaita-dark";
+    };
+    
+    font = {
+      name = "sans-serif";
+      size = 16;
+    };
+    
+    corner_radius = 8;
+    window_opacity = 0.9;
+  };
+};
 
   services.udisks2.enable = true;
   services.gvfs.enable = true;
