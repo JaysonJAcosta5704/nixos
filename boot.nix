76deb055467b56afbeabd03b9f4fad90d3a82176
Jayson Acosa <JaysonJAcosta5704.coding@gmail.com>
2025-09-06 15:32:15 -0500

    Add option for Ventoy to grub

--- boot.nix
+++ boot.nix
@@ -8,15 +8,39 @@
         device = "nodev";
         useOSProber = true;
         extraEntries = ''
-          menuentry "Shutdown" { halt }
-          menuentry "Reboot" { reboot }
-          menuentry "Enter BIOS" { fwsetup }
-          menuentry "Rescue Shell" {
-            linux /boot/bzImage
-            initrd /boot/initrd
-            boot
-          }
-        '';
+menuentry "Shutdown" { halt }
+menuentry "Reboot" { reboot }
+menuentry "Enter BIOS" { fwsetup }
+menuentry "Rescue Shell" {
+  linux /boot/bzImage
+  initrd /boot/initrd
+  boot
+}
+menuentry "Ventoy USB (/dev/sdb)" {
+    insmod part_gpt
+    insmod part_msdos
+    
+    if search --no-floppy --set=root --label "VTOYEFI"; then
+        echo "Ventoy drive found with label VTOYEFI"
+        chainloader /EFI/BOOT/BOOTX64.EFI
+        boot
+    elif search --no-floppy --set=root --label "VentoyEFI"; then
+        echo "Ventoy drive found with label VentoyEFI"
+        chainloader /EFI/BOOT/BOOTX64.EFI
+        boot
+    elif search --no-floppy --set=root --label "Ventoy"; then
+        echo "Ventoy drive found with label Ventoy"
+        chainloader /EFI/BOOT/BOOTX64.EFI
+        boot
+    else
+        echo "Ventoy drive not found"
+        echo "Insert ventoy drive before grub loads"
+        sleep 3
+        reboot
+    fi
+    
+}
+'';
       };
       efi.efiSysMountPoint = "/boot";
     };
