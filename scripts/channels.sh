5d4d047d2665475986f29901d3b05751933d96e8
Jayson Acosta <JaysonJAcosta5704.coding@gmail.com>
2025-09-07 00:57:08 -0500

    Added sudo and echos to make it more clear what is happening

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
-nix-channel --update
+echo "Adding home manager channel"
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+
+echo "Updating channels"
+sudo nix-channel --update
+sudo nix-channel --list
